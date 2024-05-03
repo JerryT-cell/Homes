310137bbc9f6bb8ed1cb512864a7ecc98cbc83fe
Jerry Takou <vianneytakou@gmail.com>
2024-05-03 12:11:13 +0200

    continue project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,11 @@ import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive,FormsModule, HomeComponent
+  imports: [CommonModule, RouterLink, RouterLinkActive,FormsModule, HomeComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'home-app';
-  fontSizePx = 16;
 }
